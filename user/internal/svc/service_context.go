2c463cd814d7cecd23cce407b8313b11b45d8526
wyj <y318224178@dingtalk.com>
2025-04-03 16:52:44 +0800

    feat: 新增用户服务模块

---
+++ user/internal/svc/service_context.go
@@ -0,0 +1,13 @@
+package svc
+
+import "grpc_demo_server/user/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
